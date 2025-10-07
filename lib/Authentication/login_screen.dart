c69e55a43296915e5dd1bd54a1ac5eb970fef7de
JMakoti <josephmwamuye11@gmail.com>
2025-10-07 23:17:12 +0300

    feat(auth): implement user signup and login flow

---
+++ lib/Authentication/login_screen.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import './signup_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // assigns a unique identifier to your Form.
+  final _formKey = GlobalKey<FormState>();
+  // Create a text controller and use it to retrieve the current value
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  bool _isPasswordVisible = false;
+
+  // submit form
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // All inputs valid – handle sign-up logic here
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Loging in......')));
+    }
+  }
+
+  // Email validation
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) return 'Please enter your email';
+    const pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+    if (!RegExp(pattern).hasMatch(value)) return 'Enter a valid email address';
+    return null;
+  }
+
+  // Password validation
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) return 'Please enter a password';
+    if (value.length < 6) return 'Password must be at least 6 characters';
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(child: _buildAccountForm()),
+    );
+  }
+
+  SingleChildScrollView _buildAccountForm() {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(24),
+      child: Form(
+        key: _formKey,
+        //flexiable for , expanded to prevent machine overflow
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //header
+            const Text(
+              "Welcome Back",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              "Sign in to continue your bursary-journey",
+              style: TextStyle(fontSize: 16, color: Colors.black),
+            ),
+            const SizedBox(height: 48),
+
+            // Email Field
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                hintText: "Email",
+                prefixIcon: Icon(Icons.email),
+              ),
+              validator: _validateEmail,
+            ),
+            const SizedBox(height: 24),
+
+            //Password Field
+            TextFormField(
+              controller: _passwordController,
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility_off
+                        : Icons.visibility,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+              ),
+              validator: _validatePassword,
+            ),
+            const SizedBox(height: 24),
+
+            //Login button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: _submitForm,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(vertical: 14),
+                ),
+                child: const Text("Log In"),
+              ),
+            ),
+
+            //Sign up Link
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Create A New Account?",
+                  style: TextStyle(color: Colors.black),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Navigate to Login Screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SignupScreen(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
